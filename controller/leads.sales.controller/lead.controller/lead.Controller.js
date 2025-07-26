76e70b704348989fc1dfc52632d910564d6c8965
Suheldevs <mohdsuhel.dev@gmail.com>
2025-07-26 12:57:22 +0530

    hsdakjf

--- controller/leads.sales.controller/lead.controller/lead.Controller.js
+++ controller/leads.sales.controller/lead.controller/lead.Controller.js
@@ -15,7 +15,7 @@ export const createLead = async (req, res) => {
       });
     }
     const lead = await leadModel.create(req.body);
-    res.status(404).json({
+    res.status(200).json({
       success: true,
       message: "Lead created successfully",
       data: lead,
